d98145e344edade8dd8f4ff5f7ab357c84236b2b
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-05-18 20:49:11 +0430

    chore: Update db config to postgresql

--- database.py
+++ database.py
@@ -3,11 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@fastapi_udemy_db/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
